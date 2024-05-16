5e1f322a756b48c490c25855d2d5db5713207fdb
yutaokkots <yuta.okkotsu@gmail.com>
2024-05-15 23:52:25 -0600

    Updated database_spliter.sh file to include config file.

--- script/database_splitter.sh
+++ script/database_splitter.sh
@@ -1,13 +1,39 @@
+## Splits a database into chunks of specific sizes and creates a config file. 
+#  see phirensky's repo: https://github.com/phiresky/sql.js-httpvfs/blob/master/create_db.sh
+
+# "e" <- script will exit immediately if any command fails
+# "u" <- produce an error if an unset variable is referenced
+set -eu
+
 # source file
 input_db="scripts/sqlite/data/master_ver3.db"
 # destination path
-output_directory="scripts/sqlite/data//master_ver3_chunked_db/"
+output_dir="scripts/sqlite/data/master_ver3_chunked_db/"
 
-# size of chunks, 5MB
-chunk_size=$((5 * 1024 * 1024))
+bytes=$(wc -c < "$input_db")
 
-# creates directory at destination path
-mkdir -p "$output_directory"
+# size of chunks, 10MB
+chunk_size=$((10 * 1024 * 1024))
+
+suffixLength=3
+
+# creates dir at destination path
+mkdir -p "$output_dir"
 
 # splits the file into chunks of 'chunk_size'
-split -b "${chunk_size}" "$input_db" "$output_directory/chunk"
+#split -b "${chunk_size}" "$input_db" "$output_dir/chunk"
+# split "$input_db" --bytes=$chunk_size "$output_dir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
+split -b "${chunk_size}" -a "$suffixLength" "$input_db" "$output_dir/db.sqlite3."
+
+requestChunkSize="$(sqlite3 "$input_db" 'pragma page_size')"
+
+echo '
+{
+    "serverMode": "chunked",
+    "requestChunkSize": '$requestChunkSize',
+    "databaseLengthBytes": '$bytes',
+    "serverChunkSize": '$chunk_size',
+    "urlPrefix": "db.sqlite3.",
+    "suffixLength": '$suffixLength'
+}
+' > "$output_dir/config.json"
